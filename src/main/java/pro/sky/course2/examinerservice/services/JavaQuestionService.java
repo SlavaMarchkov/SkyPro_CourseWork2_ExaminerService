4ab0dee32839bdc52afe0ad8ada14d8e8e494342
Viacheslav Marchkov <slavamarchkov@gmail.com>
2023-06-01 17:37:06 +0700

    Реализован сервис JavaQuestionService

---
+++ src/main/java/pro/sky/course2/examinerservice/services/JavaQuestionService.java
@@ -0,0 +1,47 @@
+package pro.sky.course2.examinerservice.services;
+
+import pro.sky.course2.examinerservice.models.Question;
+
+import java.util.*;
+
+public class JavaQuestionService implements QuestionService {
+
+    private final Set<Question> questions;
+    private final Random random = new Random();
+
+    public JavaQuestionService() {
+        this.questions = new HashSet<>();
+    }
+
+    @Override
+    public Question add(String question, String answer) {
+        Question q = new Question(question, answer);
+        questions.add(q);
+        return q;
+    }
+
+    @Override
+    public Question add(Question question) {
+        questions.add(question);
+        return question;
+    }
+
+    @Override
+    public Question remove(Question question) {
+        questions.remove(question);
+        return question;
+    }
+
+    @Override
+    public Collection<Question> getAll() {
+        return Collections.unmodifiableSet(questions);
+    }
+
+    @Override
+    public Question getRandomQuestion() {
+        int randomIndex = random.nextInt(questions.size());
+        List<Question> questionList = new ArrayList<>(questions);
+        return questionList.get(randomIndex);
+    }
+
+}
